3ab3c8ee0cdd3fa6178fd9cb6a9e3ce6f7313a16
surender singh <singhsurender28231@gmail.com>
2022-07-09 18:17:40 +0530

    git

--- src/selectors.ts
+++ src/selectors.ts
@@ -4,7 +4,6 @@ export const showsSelector=(s:State)=>{
 const showsIds =  s.shows.showsAgainstQuery[s.shows.showsQuery] || [];
 
 return showsIds.map(id => s.shows.entities[id])
-
 };
 
 export const showQuerySelector=(s: State)=> s.shows.showsQuery;
